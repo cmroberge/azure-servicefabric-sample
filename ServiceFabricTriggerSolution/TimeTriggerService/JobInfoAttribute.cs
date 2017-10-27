30f42fce0080863d5689267b7694ec3df37dc7ca
Tito Dakshinamoorthy <titod@microsoft.com>
2017-10-28 01:10:50 +0530

    time trigger service added

---
+++ ServiceFabricTriggerSolution/TimeTriggerService/JobInfoAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TimeTriggerService
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class JobInfoAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string CronExpression { get; set; }
+    }
+}
---
+++ ServiceFabricTriggerSolution/TimeTriggerService/SampleJob1.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using System;
+
+namespace TimeTriggerService
+{
+    [JobInfo(CronExpression = "0 0/1 * 1/1 * ? *", Name = "SampleJob1")]
+    public class SampleJob1 : IJob
+    {
+        private ILogger logger;
+        public SampleJob1(ILogger loggerParam)
+        {
+            this.logger = loggerParam;
+        }
+        public void Execute(IJobExecutionContext context)
+        {
+            this.logger.WriteLine($"SampleJob1 executed : {DateTime.Now} .....");
+        }
+    }
+}
